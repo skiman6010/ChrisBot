d75186d5aba50b773948fb6a8cef2219f0936f29
skiman6010 <skiman6010@gmail.com>
2016-07-15 23:22:15 -0400

    Testing out some new stuff with Nodejs

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const Script = require('smooch-bot').Script;
 
 const scriptRules = require('./script.json');
 
-
+bot.onText(/^\/say_hello (.+)$/, function (msg, match) {
+  var name = match[1];
+  bot.sendMessage(msg.chat.id, 'Hello ' + name + '!').then(function () {
+    // reply sent!
+  });
+});
 
 module.exports = new Script({
     processing: {
